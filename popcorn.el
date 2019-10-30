46fa6cb3324ffca7408ae66d7489ea2f11fb94ad
dptd <dptd@protonmail.com>
2019-10-30 08:04:07 +0100

    initial commit

---
+++ popcorn.el
@@ -0,0 +1,28 @@
+;;; popcorn.el --- Fetch movie information from TheMovieDB
+
+;; Author: dptd <dptd@protonmail.com>
+;; Keywords: movies
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;; Popcorn uses TheMovieDB database to display information about movies.
+;; https://www.themoviedb.org/
+
+;;; Code:
+
+(provide 'popcorn)
+;;; popcorn.el ends here
