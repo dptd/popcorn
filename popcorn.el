ffd8b92d43445d4593b57348e02333a89531932a
dptd <dptd@protonmail.com>
2019-10-30 08:45:08 +0100

    add popcorn group, movie struct and simple print

--- popcorn.el
+++ popcorn.el
@@ -23,6 +23,64 @@
 ;; https://www.themoviedb.org/
 
 ;;; Code:
+(require 'json)
+
+;; Custom Variables
+(defgroup popcorn nil
+  "Your movies database interface."
+  :group 'convenience
+  :prefix "popcorn-")
+
+(defcustom popcorn-api-key ""
+  "Key used to access TMDB API."
+  :type 'string)
+
+;; Interactive Function
+(defun popcorn (title)
+  "Displays information about TITLE in a new buffer."
+  (interactive "sMovie title: ")
+  (if (string-empty-p title)
+      (message "Title cannot be empty.")
+    (popcorn-display title)))
+
+;; Implementation
+(defun popcorn-display (title)
+  "Displays information about TITLE."
+  (popcorn-movie-window-print popcorn-test-movie))
+
+(cl-defstruct (popcorn-movie (:constructor popcorn-movie-create))
+  "Structure for representing single movie."
+  poster title rating votes released runtime url budget revenue imdb tagline overview)
+
+(defvar popcorn-test-movie
+  (popcorn-movie-create :poster nil
+                        :title "TestTitle"
+                        :rating 8
+                        :votes 42
+                        :released "2019.10.30"
+                        :runtime 120
+                        :url nil
+                        :budget 100
+                        :revenue 50
+                        :imdb nil
+                        :tagline nil
+                        :overview "Test overview."))
+
+;; Prints
+(defun popcorn-movie-window-print (movie)
+  "Display results for MOVIE."
+  (message (concatenate 'string
+                        "   Title: " (popcorn-movie-title movie) "\n"
+                        "  Rating: " (number-to-string (popcorn-movie-rating movie)) " (" (number-to-string (popcorn-movie-votes movie)) " votes)\n"
+                        "Released: " (popcorn-movie-released movie) "\n"
+                        " Runtime: " (number-to-string (popcorn-movie-runtime movie)) " minutes\n"
+                        " Website: " (popcorn-movie-url movie) "\n"
+                        "  Budget: $" (number-to-string (popcorn-movie-budget movie)) "\n"
+                        " Revenue: $" (number-to-string (popcorn-movie-revenue movie)) "\n"
+                        "    IMDB: " (popcorn-movie-imdb movie) "\n"
+                        " Tagline: " (popcorn-movie-tagline movie) "\n"
+                        "Overview: " (popcorn-movie-overview movie) "\n")))
 
 (provide 'popcorn)
 ;;; popcorn.el ends here
+
